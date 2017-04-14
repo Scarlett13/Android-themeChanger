9738d3045d1ff934d6e44cdb6cfd69525cfd7750
Scarlett13 <professionalone6@gmail.com>
2017-04-14 21:56:56 +0800

    Here lies my newbie android studio program for changing the theme...

---
+++ app/src/main/java/com/example/scarlett/themechanger/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.scarlett.themechanger;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    /**
+     * Called when the activity is first created.
+     */
+
+    @Override
+
+    public void onCreate(Bundle savedInstanceState)
+
+    {
+        super.onCreate(savedInstanceState);
+        themeUtils.onActivityCreateSetTheme(this);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.ravenclaw).setOnClickListener(this);
+        findViewById(R.id.gold).setOnClickListener(this);
+        findViewById(R.id.green).setOnClickListener(this);
+        findViewById(R.id.gryffindor).setOnClickListener(this);
+        findViewById(R.id.hufflepuff).setOnClickListener(this);
+        findViewById(R.id.slytherin).setOnClickListener(this);
+        findViewById(R.id.pastel).setOnClickListener(this);
+
+    }
+
+    @Override
+
+    public void onClick(View v)
+
+    {
+        switch (v.getId()) {
+            case R.id.ravenclaw:
+                themeUtils.changeToTheme(this, themeUtils.rc);
+                break;
+            case R.id.gold:
+                themeUtils.changeToTheme(this, themeUtils.gol);
+                break;
+            case R.id.green:
+                themeUtils.changeToTheme(this, themeUtils.green);
+                break;
+            case R.id.gryffindor:
+                themeUtils.changeToTheme(this, themeUtils.gr);
+                break;
+            case  R.id.pastel:
+                themeUtils.changeToTheme(this, themeUtils.pt);
+                break;
+            case R.id.slytherin:
+                themeUtils.changeToTheme(this, themeUtils.sl);
+                break;
+            case R.id.hufflepuff:
+                themeUtils.changeToTheme(this, themeUtils.hp);
+                break;
+            default:
+                themeUtils.changeToTheme(this, themeUtils.green);
+                break;
+
+        }
+    }
+}
